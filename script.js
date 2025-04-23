ea7f5f2c9e9bb8fc3c9df1a9f8a1aa71f13adba4
kanyasrithong <ks4264@drexel.edu>
2025-04-23 10:33:08 -0400

    initialize script.js

--- script.js
+++ script.js
@@ -4,12 +4,16 @@ let currentValue = 0;
 // calls display number as string
 const displayText = document.querySelector('.display-text');
 
-// sets default temporary value for display as integer (currentValue cannot rely on displayText.innerText)
+// store display value as integer
 let displayValue = 0;
+
+// store operation performed between currentValue and operantValue
+let operationType = '';
 
 const clear = document.querySelector('.clear');
 const back = document.querySelector('.back');
 
+const zero = document.querySelector('.zero');
 const one = document.querySelector('.one');
 const two = document.querySelector('.two');
 const three = document.querySelector('.three');
@@ -25,6 +29,12 @@ const minus = document.querySelector('.minus');
 const multiply = document.querySelector('.multiply');
 const divide = document.querySelector('.divide');
 const equal = document.querySelector('.equal');
+
+zero.addEventListener('click', () => {
+  inputNumber(zero);
+  console.log(currentValue);
+})
+
 
 one.addEventListener('click', function() {
   inputNumber(one);
@@ -71,19 +81,88 @@ nine.addEventListener('click', function() {
   console.log(currentValue);
 });
 
+
+plus.addEventListener('click', function() {
+  // set operation type
+  operationType = plus.innerText;
+  operationPerformed(operationType);
+  console.log(`displayValue: ${displayValue}`);
+  console.log(`currentValue: ${currentValue}`);
+  console.log(`operationType: ${operationType}`);
+});
+
+multiply.addEventListener('click', function() {
+  // set operation type
+  operationType = multiply.innerText;
+  operationPerformed(operationType);
+  console.log(`displayValue: ${displayValue}`);
+  console.log(`currentValue: ${currentValue}`);
+  console.log(`operationType: ${operationType}`);
+});
+
+equal.addEventListener('click', function() {
+  displayText.innerText = operationPerformed(operationType);
+  // reset operationType to prevent repeat calculation on multiple clicks
+  operationType = '';
+  console.log(`displayValue: ${displayValue}`);
+  console.log(`currentValue: ${currentValue}`);
+  console.log(`operationType: ${operationType}`);
+});
+
+clear.addEventListener('click', function() {
+  displayText.innerText = 0;
+  currentValue = 0;
+})
+
 function inputNumber(num) {
+  // set inputValue to button number
   let inputValue = parseInt(num.innerText);
-  if (displayValue === 0) {
+  if (displayText.innerText === '0') {
     displayValue = inputValue;
-    displayText.innerText = inputValue;
+    displayText.innerText = displayValue;
     console.log('replaced!');
+    console.log(`displayValue: ${displayValue}`);
+    console.log(`currentValue: ${currentValue}`);
+    console.log(`operationType: ${operationType}`);
   } else {
     // append num to end of displayed text
     displayText.innerText += inputValue;
     // change number value of displayed text
     displayValue = parseInt(displayText.innerText);
     console.log('appended!');
+    console.log(`displayValue: ${displayValue}`);
+    console.log(`currentValue: ${currentValue}`);
+    console.log(`operationType: ${operationType}`);
   }
 }
 
+// detects operation type + uses it after equal sign is clicked
+function operationPerformed(operation) {
+  if (currentValue === 0){
+    currentValue = displayValue;
+    displayText.innerText = 0;
+  } else {
+    switch (operation) {
+      case '+':
+        currentValue += displayValue;
+        console.log(`kanya: ${currentValue}`);
+        displayText.innerText = 0;
+        console.log(`plus`);
+        break;
+      case '-':
+        currentValue -= displayValue;
+        displayText.innerText = 0;
+        break;
+      case '×':
+        currentValue *= displayValue;
+        displayText.innerText = 0;
+        break;
+      case '÷':
+        currentValue /= displayValue;
+        displayText.innerText = 0;
+        break;
+    }
+  }
+  return currentValue;
+}
 
